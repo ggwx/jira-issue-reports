79d5257b8878e64e3a454a6fb58beb76b039ee44
liuyuancheng <tenstone@foxmail.com>
2018-03-28 14:20:33 +0800

    fixed untracked files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8-*-
 from jira import JIRA
 
-jira = JIRA(server='http://jira.lanzhu.xin',basic_auth=('tenstone', 'Tenstone@123'))
-
-print(jira)
+jira = JIRA(server='http://jira.lanzhu.xin', basic_auth=('tenstone', ''))
+projects = jira.projects()
+print(projects.encode('utf8'))
